9d3c9ff5a79d955def7b5511e595e7e3e86bc99b
jitash <jitash@foxmail.com>
2021-07-12 17:24:41 +0800

    Lecture2_Learning more about SwiftUI

--- CS193p-Memorize/ContentView.swift
+++ CS193p-Memorize/ContentView.swift
@@ -8,9 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let emojis=["🚗","🚕","🚙","🚌","🚎","🏎","🚓","🚑","🚒","🚐","🛻","🚚","🚛","🚜","🦯","🦽","🦼","🛴","🚲","🛵","🏍","🛺","🚨","🚔"]
+    @State var emojiCount = 4
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            ScrollView{
+                
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 85))], content: {
+                    ForEach(emojis[0..<emojis.count],id:\.self){emoji in
+                        CardView(content: emoji)
+                            .aspectRatio(2/3,contentMode: .fit)
+                    }
+                })
+            }
+            Spacer()
+            HStack{
+                remove
+                Spacer()
+                add
+            }
+            
+        }.padding(.all)
+    }
+    var remove:some View{
+        Button(action: {
+            if emojiCount > 0 {
+                emojiCount -= 1
+            }
+        }, label: {
+            Image(systemName: "minus.circle")
+        })
+    }
+    var add:some View{
+        Button(action: {
+            if emojiCount < emojis.count{
+                emojiCount += 1
+            }
+        }, label: {
+            Image(systemName: "plus.circle")
+        })
+    }
+}
+
+struct CardView:View {
+    var content:String
+    @State var isFaceUp = false
+    
+    var body: some View{
+        ZStack{
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp{
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            }else{
+                shape.fill()
+            }
+        }.onTapGesture {
+            isFaceUp = !isFaceUp
+        }
     }
 }
 
