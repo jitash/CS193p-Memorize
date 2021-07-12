965d76fee510a13ed4e8ddd03f10d47ea3a2915a
jitash <jitash@foxmail.com>
2021-07-12 16:44:54 +0800

    Initial Commit

---
+++ CS193p-Memorize/CS193p_MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS193p_MemorizeApp.swift
+//  CS193p-Memorize
+//
+//  Created by jitash on 2021/7/12.
+//
+
+import SwiftUI
+
+@main
+struct CS193p_MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CS193p-Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CS193p-Memorize
+//
+//  Created by jitash on 2021/7/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
